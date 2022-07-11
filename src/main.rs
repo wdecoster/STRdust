9ed865959470ce24be048917676e2eca46edb623
wdecoster <decosterwouter@gmail.com>
2022-07-11 08:25:01 +0200

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rust_htslib::{bam, bam::Read};
+
+
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn parse_insertions_from_bam(bampath) {
+    let bam = bam::Reader::from_path(bampath).unwrap();
+    for r in bam.records() {
+        let record = r.unwrap();
+        if record.is_reverse() {
+            out.write(&record).unwrap();
+        }
+    }
 }
