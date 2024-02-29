be00e038b6dfb4b8915e388863537059ce436281
wdecoster <decosterwouter@gmail.com>
2024-02-29 11:51:41 +0100

    major refactoring

--- src/call.rs
+++ src/call.rs
@@ -9,103 +9,55 @@ use crate::{genotype, parse_bam, Cli};
 pub fn genotype_repeats(args: Cli) {
     debug!("Genotyping STRs in {}", args.bam);
 
-    match (&args.region, &args.region_file) {
-        // both input options are specified: invalid
-        (Some(_region), Some(_region_file)) => {
-            error!("ERROR: Specify either a region (-r) or region_file (-R), not both!\n\n");
-            panic!();
-        }
-        // no input options are specified: invalid
-        (None, None) => {
-            error!("ERROR: Specify one of region (-r) or region_file (-R)!\n\n");
-            panic!();
-        }
+    let repeats = match (&args.region, &args.region_file, args.pathogenic) {
         // a region string is specified: single threaded
-        (Some(region), None) => {
-            let repeat = crate::repeats::RepeatInterval::from_string(region, &args.fasta);
-            let mut bam = parse_bam::create_bam_reader(&args.bam, &args.fasta);
-            if let Some(repeat) = repeat {
-                if let Ok(output) =
-                    genotype::genotype_repeat_singlethreaded(&repeat, &args, &mut bam)
-                {
-                    crate::vcf::write_vcf_header(&args.fasta, &args.bam, &args.sample);
-                    println!("{output}");
-                }
-            } else {
-                // None is returned when the interval from the bed file does not appear in the fasta file
-                error!("Interval {region} not found in the fasta file, ignoring...");
-            }
+        (Some(region), None, false) => {
+            crate::repeats::RepeatIntervalIterator::from_string(region, &args.fasta)
         }
         // a region file is specified: single or multithreaded
-        (None, Some(region_file)) => {
-            // TODO: check if bed file is okay?
-            let mut reader =
-                bed::Reader::from_file(region_file).expect("Problem reading bed file!");
-            crate::vcf::write_vcf_header(&args.fasta, &args.bam, &args.sample);
-            let stdout = io::stdout(); // get the global stdout entity
-            let mut handle = io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
-
-            // if multithreaded, create a threadpool
-            if args.threads > 1 {
-                rayon::ThreadPoolBuilder::new()
-                    .num_threads(args.threads)
-                    .build()
-                    .expect("Failed to create threadpool");
-                // genotypes contains the output of the genotyping, a struct instance
-                let genotypes = Mutex::new(Vec::new());
-                // par_bridge does not guarantee that results are returned in order
-                reader.records().par_bridge().for_each(|record| {
-                    let rec: bed::Record = record.expect("Error reading bed record.");
-                    let repeat = crate::repeats::RepeatInterval::from_bed(&rec, &args.fasta);
-                    if let Some(repeat) = repeat {
-                        if let Ok(output) = genotype::genotype_repeat_multithreaded(&repeat, &args)
-                        {
-                            let mut geno =
-                                genotypes.lock().expect("Unable to lock genotypes mutex");
-                            geno.push(output);
-                        } else {
-                            error!("Problem processing {repeat}");
-                        }
-                    } else {
-                        error_invalid_interval(&rec);
-                    }
-                });
-                let mut genotypes_vec = genotypes.lock().unwrap();
-                // The final output is sorted by chrom, start and end
-                genotypes_vec.sort_unstable();
-                for g in &mut *genotypes_vec {
-                    writeln!(handle, "{g}").expect("Failed writing the result.");
-                }
-            } else {
-                // When running single threaded things become easier and the tool will require less memory
-                // Output is returned in the same order as the bed, and therefore not sorted before writing immediately to stdout
-                // The indexedreader is created once and passed on to the function
-                let mut bam = parse_bam::create_bam_reader(&args.bam, &args.fasta);
-                // genotypes contains the output of the genotyping, a struct instance
-                for record in reader.records() {
-                    let rec = record.expect("Error reading bed record.");
-                    let repeat = crate::repeats::RepeatInterval::from_bed(&rec, &args.fasta);
-                    if let Some(repeat) = repeat {
-                        if let Ok(output) =
-                            genotype::genotype_repeat_singlethreaded(&repeat, &args, &mut bam)
-                        {
-                            writeln!(handle, "{output}").expect("Failed writing the result.");
-                        }
-                    } else {
-                        error_invalid_interval(&rec);
-                    }
-                }
+        (None, Some(region_file), false) => {
+            crate::repeats::RepeatIntervalIterator::from_bed(region_file, &args.fasta)
+        }
+        (None, None, true) => crate::repeats::RepeatIntervalIterator::pathogenic(&args.fasta),
+        _ => {
+            error!("ERROR: Specify either a region (-r), a region_file (-R) or --pathogenic!\n\n");
+            panic!();
+        }
+    };
+    crate::vcf::write_vcf_header(&args.fasta, &args.bam, &args.sample);
+    let stdout = io::stdout(); // get the global stdout entity
+    let mut handle = io::BufWriter::new(stdout); // wrap that handle in a buffer
+    if args.threads == 1 {
+        // When running single threaded things become easier and the tool will require less memory
+        // Output is returned in the same order as the bed, and therefore not sorted before writing immediately to stdout
+        // The indexedreader is created once and passed on to the function
+        let mut bam = parse_bam::create_bam_reader(&args.bam, &args.fasta);
+        for repeat in repeats {
+            if let Ok(output) = genotype::genotype_repeat_singlethreaded(&repeat, &args, &mut bam) {
+                writeln!(handle, "{output}").expect("Failed writing the result.");
             }
         }
+    } else {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(args.threads)
+            .build()
+            .expect("Failed to create threadpool");
+        // genotypes contains the output of the genotyping, a struct instance
+        let genotypes = Mutex::new(Vec::new());
+        // par_bridge does not guarantee that results are returned in order
+        repeats.par_bridge().for_each(|repeat| {
+            if let Ok(output) = genotype::genotype_repeat_multithreaded(&repeat, &args) {
+                let mut geno = genotypes.lock().expect("Unable to lock genotypes mutex");
+                geno.push(output);
+            } else {
+                error!("Problem processing {repeat}");
+            }
+        });
+        let mut genotypes_vec = genotypes.lock().unwrap();
+        // The final output is sorted by chrom, start and end
+        genotypes_vec.sort_unstable();
+        for g in &mut *genotypes_vec {
+            writeln!(handle, "{g}").expect("Failed writing the result.");
+        }
     }
-}
-// None is returned when the interval from the bed file does not appear in the fasta file
-// Alternatively, this error can be raised if the end is smaller than the start
-fn error_invalid_interval(rec: &bed::Record) {
-    error!(
-        "ERROR: Invalid interval in bed file: {chrom}:{begin}-{end}",
-        chrom = rec.chrom(),
-        begin = rec.start(),
-        end = rec.end()
-    );
 }
